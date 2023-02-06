31dbc5cc1a0daa2ddfcb6591634a7b6229f7d944
Eric Schmidt <schmidte@aetna.com>
2023-02-06 15:44:03 -0700

    change to layer updates

--- config/boards/arm/nice_nano/hello_widget.c
+++ config/boards/arm/nice_nano/hello_widget.c
@@ -11,10 +11,16 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
+#include <zmk/display/widgets/layer_status.h>
 
 struct zmk_widget_hello {
     sys_snode_t node;
     lv_obj_t *obj;
+};
+
+struct layer_status_state {
+    uint8_t index;
+    const char *label;
 };
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
@@ -23,8 +29,9 @@ struct hello_state {
     bool connected;
 };
 
-static struct hello_state get_state(const zmk_event_t *_eh) {
-    return (struct hello_state){.connected = zmk_split_bt_peripheral_is_connected()};
+static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
+    uint8_t index = zmk_keymap_highest_layer_active();
+    return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_label(index)};
 }
 
 static void output_status_update_cb(struct hello_state state) {
@@ -34,7 +41,7 @@ static void output_status_update_cb(struct hello_state state) {
 
 ZMK_DISPLAY_WIDGET_LISTENER(widget_hello, struct hello_state,
                             hello_update_cb, get_state)
-ZMK_SUBSCRIPTION(widget_hello, zmk_split_peripheral_status_changed);
+ZMK_SUBSCRIPTION(widget_hello, zmk_layer_state_changed);
 
 int zmk_widget_hello_init(struct zmk_widget_hello *widget,
                                       lv_obj_t *parent) {
