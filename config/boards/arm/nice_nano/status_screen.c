bfd47d11075cd131a98ba1958534e2316afe2f6e
Eric Schmidt <schmidte@aetna.com>
2023-02-08 11:31:31 -0700

    add center frame

--- config/boards/arm/nice_nano/status_screen.c
+++ config/boards/arm/nice_nano/status_screen.c
@@ -19,7 +19,20 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 lv_obj_t *zmk_display_status_screen() {
 
     lv_obj_t *screen;
+    lv_obj_t *center_frame;
+
+    lv_style_init(&global_style);
+    lv_style_set_text_font(&global_style, LV_STATE_DEFAULT, &lv_font_montserrat_26);
+    lv_style_set_text_letter_space(&global_style, LV_STATE_DEFAULT, 1);
+    lv_style_set_text_line_space(&global_style, LV_STATE_DEFAULT, 1);
     screen = lv_obj_create(NULL, NULL);
+    lv_obj_add_style(screen, LV_LABEL_PART_MAIN, &global_style);
+
+    center_frame = lv_cont_create(screen, NULL);
+    lv_obj_set_auto_realign(center_frame, true);
+    lv_obj_align(center_frame, NULL, LV_ALIGN_CENTER, 0, 0);
+    lv_cont_set_fit(center_frame, LV_FIT_TIGHT);
+    lv_cont_set_layout(center_frame, LV_LAYOUT_CENTER);
 
     bongo_cat_widget->obj = lv_label_create(screen, NULL);
     lv_obj_set_size(bongo_cat_widget->obj, 40, 15);
